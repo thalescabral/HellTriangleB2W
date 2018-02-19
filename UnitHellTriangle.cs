9638f2935342b8bc7718232df3aab28088a77c61
thalescabral <33131429+thalescabral@users.noreply.github.com>
2018-02-19 18:37:15 -0300

    Add files via upload

---
+++ UnitHellTriangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestHellTriangleB2W
+{
+    [TestClass]
+    public class UnitHellTriangle
+    {
+        int[][] triangle = {
+                new int[] { 6 },
+                new int[] { 3, 5 },
+                new int[] { 9, 7, 1 },
+                new int[] { 4, 6, 8, 4 },
+            };
+
+        [TestMethod]
+        public void TestSuccess()
+        {
+           Int32 resultSucess = 26;
+           Int32 resultMethod = HellTriangleB2WApp.Program.UserLinq(triangle);
+           Assert.IsTrue(resultSucess == resultMethod);
+        }
+
+    }
+}
